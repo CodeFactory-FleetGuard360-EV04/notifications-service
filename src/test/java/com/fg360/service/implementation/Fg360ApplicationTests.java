8b56f6bfcc870580ef9c6d9e5c44437aad58a81b
LeviTorresx <levist42@gmail.com>
2025-11-03 18:01:53 -0500

    QA CONFIG: unit test, template [workflow-sonarqube]

---
+++ src/test/java/com/fg360/service/implementation/Fg360ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fg360.service.implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.profiles.active=test")
+class Fg360ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
