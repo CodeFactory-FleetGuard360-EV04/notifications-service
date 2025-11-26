418d5e73ed5f86ec8ed3466ed271c9419a998669
LeviTorresx <levist42@gmail.com>
2025-11-25 22:10:31 -0500

    Fix beans spring

--- src/test/java/com/fg360/service/implementation/Fg360ApplicationTests.java
+++ src/test/java/com/fg360/service/implementation/Fg360ApplicationTests.java
@@ -3,7 +3,7 @@ package com.fg360.service.implementation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+
 class Fg360ApplicationTests {
 
     @Test
